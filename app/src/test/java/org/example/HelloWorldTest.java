8a79d904882111c42d9eb17fa45d7530737cdecc
Elimka777 <elimazholdubaeva@gmail.com>
2025-08-24 20:51:48 -0500

    Update

--- app/src/test/java/org/example/HelloWorldTest.java
+++ app/src/test/java/org/example/HelloWorldTest.java
@@ -3,10 +3,10 @@ package org.example;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-class GreeterTest {
+class HelloWorldTest {
   @Test
   void itHasAGreeting() {
-    Greeter greeter = new Greeter();
-    assertEquals(greeter.getGreeting(), "Hello World!");
+    HelloWorld helloWorld = new HelloWorld();
+    assertNotNull(helloWorld.getGreeting());
   }
 }
